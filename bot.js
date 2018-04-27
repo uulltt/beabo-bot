1ba8403a6bb890bf52bc7cda590a60153f54c4b1
uulltt <ajvark@gmail.com>
2018-04-27 19:59:52 -0400

    fuck you github long live gitlab

---
+++ bot.js
@@ -0,0 +1,33 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 5) == '!list') {
+        var args = message.substring(5).split('\n');
+        var cmd = args[0];
+       for(var i = 1; i < args.length; i++){
+                bot.sendMessage({
+                    to: channelID,
+                    message: args[i]
+                }); 
+         }
+     }
+});
