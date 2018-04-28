d33148200f7016e8978e8d53ab35ce3325538919
uulltt <ajvark@gmail.com>
2018-04-27 20:08:39 -0400

    maybe now it will work?

--- bot.js
+++ bot.js
@@ -1,33 +1,19 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 5) == '!list') {
-        var args = message.substring(5).split('\n');
-        var cmd = args[0];
-       for(var i = 1; i < args.length; i++){
-                bot.sendMessage({
-                    to: channelID,
-                    message: args[i]
-                }); 
+
+client.on('message', message => {
+    if (message.content.substring(0, 5) === '!list') {
+		var args = message.content.substring(5).split('\n');
+		for(var i = 1; i < args.length; i++){
+             message.channel.send(args[i]);  
          }
-     }
+    	
+  	}
 });
+
+client.login(process.env.BOT_TOKEN);
