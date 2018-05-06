4127d6f0d06b5df3e4f010542d2c69bea2fcfb9c
uulltt <ajvark@gmail.com>
2018-05-06 09:03:37 -0400

    big update-o

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+	 client.user.setUsername("NagaevskyTron 60");
 });
 
 client.on('message', message => {
@@ -20,41 +21,88 @@ client.on('message', message => {
   	}
 	if (message.content.substring(0, 5) === '!simp'){
 		var arg = message.content.substring(6);
+		var args = message.content.substring.split('\n');
+		for(var i = 0; i < args.length; i++){
 		message.channel.send({
 			embed: {
     image: {
-         url: 'https://nfggames.com/system/arcade/arcade.php/y-simp/z-0/dbl-2/x-' + encodeURI(arg)
+         url: 'https://nfggames.com/system/arcade/arcade.php/y-simp/z-0/dbl-2/x-' + encodeURI(args[i])
       }
    }
+		
 })
   .then(console.log)
   .catch(console.error);
 	}
+}
 if (message.content.substring(0, 6) === '!kof2k'){
 		var arg = message.content.substring(7);
+		var args = message.content.substring.split('\n');
+		for(var i = 0; i < args.length; i++){
 		message.channel.send({
 			embed: {
     image: {
-         url: 'https://nfggames.com/system/arcade/arcade.php/y-KoF2k/z-6/dbl-2/x-' + encodeURI(arg)
+         url: 'https://nfggames.com/system/arcade/arcade.php/y-KoF2k/z-6/dbl-2/x-' + encodeURI(args[i])
       }
    }
 })
   .then(console.log)
   .catch(console.error);
 	}
-if (message.content.substring(0, 5) === '!pubu'){
-		var arg = message.content.substring(6);
+}
+
+if (message.content.substring(0, 6) === '!kof97'){
+		var arg = message.content.substring(7);
+		var args = message.content.substring.split('\n');
+		for(var i = 0; i < args.length; i++){
 		message.channel.send({
 			embed: {
     image: {
-         url: 'https://nfggames.com/system/arcade/arcade.php/y-pubu/z-0/dbl-2/x-' + encodeURI(arg)
+         url: 'https://nfggames.com/system/arcade/arcade.php/y-kof97/z-6/dbl-2/x-' + encodeURI(args[i])
       }
    }
 })
   .then(console.log)
   .catch(console.error);
-	}		
+	}
+}
+
+if (message.content.substring(0, 5) === '!pubu'){
+		var arg = message.content.substring(6);
+		var args = message.content.substring.split('\n');
+		for(var i = 0; i < args.length; i++){
+		message.channel.send({
+			embed: {
+    image: {
+         url: 'https://nfggames.com/system/arcade/arcade.php/y-pubu/z-0/dbl-2/x-' + encodeURI(args[i])
+      }
+   }
+})
+  .then(console.log)
+  .catch(console.error);
+	}
+}
+
+if (message.content.substring(0, 3) === '!fz'){
+		var arg = message.content.substring(4);
+		var args = message.content.substring.split('\n');
+		for(var i = 0; i < args.length; i++){
+		message.channel.send({
+			embed: {
+    image: {
+         url: 'https://nfggames.com/system/arcade/arcade.php/y-fz/z-0/dbl-2/x-' + encodeURI(args[i])
+      }
+   }
+})
+  .then(console.log)
+  .catch(console.error);
+	}
+}	
 
 });
+
+if (message.content.substring(0, 5) === '!help' || message.content.substring(0, 9) === '!commands'){
+		message.channel.send('font commands\n!fz - fantasy zone font\n!kof97 - king of fighters 97 font\n!kof2k - king of fighters 2000 font\n!pubu - puzzle bobble font\n!simp - the simpsons font\nother commands\n!list or !todo - splits discord message into a to-do list');
+}
 
 client.login(process.env.BOT_TOKEN);
