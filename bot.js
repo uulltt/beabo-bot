7a8847b1dac949d9ca42a20068574e40100413ec
uulltt <ajvark@gmail.com>
2018-05-08 12:28:01 -0400

    getting better at regex

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ client.on('message', message => {
 .then(function(result){
 	var dir = ':motorway: \"' + origin + '\" to \"' + destination + '\"\n';
 	for(var i = 0; i < result.routes[0].legs[0].steps.length; i++){
-		dir = dir + (i+1).toString() + '. ' + result.routes[0].legs[0].steps[i].html_instructions.replace(/<b>/gm, '**').replace(/<\/b>/gm, '**').replace(/<div style="font-size:0.9em">/gm, ' `(').replace(/<\/div>/gm, ')`') + ' (' + result.routes[0].legs[0].steps[i].distance.text + ', ' + result.routes[0].legs[0].steps[i].duration.text + ')\n';
+		dir = dir + (i+1).toString() + '. ' + result.routes[0].legs[0].steps[i].html_instructions.replace(/<\/?b>/gm, '**').replace(/<div style="font-size:0.9em">/gm, ' `(').replace(/<\/div>/gm, ')`') + ' (' + result.routes[0].legs[0].steps[i].distance.text + ', ' + result.routes[0].legs[0].steps[i].duration.text + ')\n';
 	}
 	dir = dir + 'And you\'re there! :smiley:';
 	if (dir.length <= 2000)
