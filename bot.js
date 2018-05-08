ca1e0456f99ca4080458841c959eb08e8487e0ad
uulltt <ajvark@gmail.com>
2018-05-08 00:52:27 -0400

    added motorway icon

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ client.on('message', message => {
   destination: dest
 })
 .then(function(result){
-	var dir = '\"' + ori + '\" to \"' + dest + '\"\n';
+	var dir = ':motorway: \"' + ori + '\" to \"' + dest + '\"\n';
 	for(var i = 0; i < result.routes[0].legs[0].steps.length; i++){
 		dir = dir + (i+1).toString() + '. ' + result.routes[0].legs[0].steps[i].html_instructions.replace(/<b>/gm, '**').replace(/<\/b>/gm, '**').replace(/<div style="font-size:0.9em">/gm, ' `(').replace(/<\/div>/gm, ')`') + ' (' + result.routes[0].legs[0].steps[i].distance.text + ', ' + result.routes[0].legs[0].steps[i].duration.text + ')\n';
 	}
