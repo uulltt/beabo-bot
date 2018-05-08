20ff98ea8b170d8a859183cfe3ce68f1126593d2
uulltt <ajvark@gmail.com>
2018-05-08 18:38:13 -0400

    maybe the presence will be fixed now

--- bot.js
+++ bot.js
@@ -28,7 +28,8 @@ client.on('ready', () => {
         game: {
         name: 'type !commands for help',
         type: 0
-    }
+    }, status: 'online'
+	}
 	client.user.setPresence(presence)
 });
 
