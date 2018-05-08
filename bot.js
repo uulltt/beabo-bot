5573d016df93215613f31a43969500de64da109d
uulltt <ajvark@gmail.com>
2018-05-08 16:17:10 -0400

    hmmm

--- bot.js
+++ bot.js
@@ -131,9 +131,11 @@ client.on('message', message => {
 		var blogId = hasBlogId[hasBlogId.length - 1];
 		var postId = message.content.substring(message.content.indexOf('/post/') + ('/post/').length).match(/[0-9]+/gm)[0];
 		tumblrClient.blogPosts(blogId, {id : postId}, function(err, resp) {
+			if (!err){
 			console.log(resp.posts);
+			}
 		}
-		).catch(console.error);
+		);
 		/*imgur.getAlbumInfo(theAlbum)
     .then(function(json) {
 		for (var i = 0; i < json.data.images.length; i++) {
