8ac79937e7f1a4b443a068edf235642f64eee105
uulltt <ajvark@gmail.com>
2018-05-08 17:39:29 -0400

    took out jquery stuff

--- bot.js
+++ bot.js
@@ -7,10 +7,6 @@ const client = new Discord.Client();
 var googlePlaces = require('googleplaces');
 var GPlaces = new googlePlaces(process.env.PLACES_KEY, "json");
 var imgur = require('imgur');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const { window } = new JSDOM(`<!DOCTYPE html>`);
-var $ = require('jquery')(window);
 
 var tumblr = require('tumblrwks');
 var tumblrClient = new tumblr({
