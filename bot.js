33b7c79a61baa066c8f298df400b3b3d5ffe202c
uulltt <ajvark@gmail.com>
2018-05-10 22:27:55 -0400

    hopefully this works

--- bot.js
+++ bot.js
@@ -322,231 +322,38 @@ client.on('message', message => {
 					collector.on('collect', message2 => {
 						if (message2.user === message.user && message2.channel === message.channel && parseInt(message2.content)) {
 							var id = json.results[parseInt(message2.content) - 1].id;
-							gbGet[g].get(id, function (err2, res2, json2) {
-								var queries = query.split(',');
-								var Name = json2.results.name;
-								var imageURL = json2.results.image.original_url;
-								for (var q = 0; q < queries.length; q++) {
-									var embedTitle = Name + ' ';
-									var embedString = '';
-									embedString += xtra.gbwiki(json2, queries[q]);
-									//var embedImage = json2.results.image.original_url;
-									if (queries[q] === 'info' && g === 0) {
-										//console.log(json2.results.original_release_date);
-
-
-										var platforms = '';
-										var genres = '';
-										var themes = '';
-										var developers = '';
-										var publishers = ''
-											var dlcs = ''
-											for (var i = 0; i < json2.results.developers.length; i++) {
-
-												if ((developers + ', [' + json2.results.developers[i].name + '](' + json2.results.developers[i].site_detail_url + ')').length < 2048) {
-													if (i > 0) {
-														developers += ', ';
-													}
-													developers += '[' + json2.results.developers[i].name + '](' + json2.results.developers[i].site_detail_url + ')';
-												}
-											}
-											for (var i = 0; i < json2.results.publishers.length; i++) {
-												if ((publishers + ', [' + json2.results.publishers[i].name + '](' + json2.results.publishers[i].site_detail_url + ')').length < 2048) {
-													if (i > 0) {
-														publishers += ', ';
-													}
-													publishers += '[' + json2.results.publishers[i].name + '](' + json2.results.publishers[i].site_detail_url + ')';
-												}
-											}
-											if (json2.results.dlcs !== undefined && json2.results.dlcs !== null && typeof json2.results.dlcs !== undefined && typeof json2.results.dlcs !== null) {
-												for (var i = 0; i < json2.results.dlcs.length; i++) {
-
-													if ((dlcs + ', [' + json2.results.dlcs[i].name + '](' + json2.results.dlcs[i].site_detail_url + ')').length < 2048 && !dlcs.includes('[' + json2.results.dlcs[i].name + '](' + json2.results.dlcs[i].site_detail_url + ')')) {
-														if (i > 0) {
-															dlcs += ', ';
-														}
-														dlcs += '[' + json2.results.dlcs[i].name + '](' + json2.results.dlcs[i].site_detail_url + ')';
-													}
-												}
-											} else {
-												dlcs = 'None';
-											}
-											for (var i = 0; i < json2.results.platforms.length; i++) {
-
-												if ((platforms + ', [' + json2.results.platforms[i].name + '](' + json2.results.platforms[i].site_detail_url + ')').length < 2048) {
-													if (i > 0) {
-														platforms += ', ';
-													}
-													platforms += '[' + json2.results.platforms[i].name + '](' + json2.results.platforms[i].site_detail_url + ')';
-												}
-											}
-
-											for (var i = 0; i < json2.results.genres.length; i++) {
-
-												if ((genres + ', [' + json2.results.genres[i].name + '](' + json2.results.genres[i].site_detail_url + ')').length < 2048) {
-													if (i > 0) {
-														genres += ', ';
-													}
-													genres += '[' + json2.results.genres[i].name + '](' + json2.results.genres[i].site_detail_url + ')';
-												}
-											}
-											if (json2.results.themes !== undefined && json2.results.themes !== null && typeof json2.results.themes !== undefined && typeof json2.results.themes !== null) {
-												for (var i = 0; i < json2.results.themes.length; i++) {
-
-													if ((themes + '[' + json2.results.themes[i].name + '](' + json2.results.themes[i].site_detail_url + ')').length < 2048) {
-														if (i > 0) {
-															themes += ', ';
-														}
-														themes += '[' + json2.results.themes[i].name + '](' + json2.results.themes[i].site_detail_url + ')';
-													}
-												}
-											} else {
-												themes = 'None';
-											}
-
-											message.channel.send({
-												embed: {
-													title: embedTitle,
-													description: embedString,
-													url: json2.results.site_detail_url,
-													footer: {
-														text: 'From Giant Bomb Wiki'
-													},
-													color: 0xa81717,
-													thumbnail: {
-														url: imageURL
-													},
-													fields: [{
-															name: "Original Release Date",
-															value: json2.results.original_release_date.substring(0, json2.results.original_release_date.indexOf(' ')),
-															inline: true
-														}, {
-															name: "Platforms",
-															value: platforms,
-															inline: true
-														}, {
-															name: "Developers",
-															value: developers,
-															inline: true
-														}, {
-															name: "Publishers",
-															value: publishers,
-															inline: true
-														}, {
-															name: "Genres",
-															value: genres,
-															inline: true
-														}, {
-															name: "Themes",
-															value: themes,
-															inline: true
-														}, {
-															name: "DLCs",
-															value: dlcs
-														},
-													]
-												}
-											});
-
-									} else {
-										if (queries[q] === 'info' && g === 1) {
-
-											if (json2.results.first_appeared_in_game !== null) {
-												embedString += '**•First Appearance: [' + json2.results.first_appeared_in_game.name + '](' + json2.results.first_appeared_in_game.site_detail_url + ')**\n';
-											}
-											var genders = ['0', 'Male', 'Female', '3'];
-											embedString += '**•Gender: ' + genders[json2.results.gender] + '**\n';
-											embedString += '**•Birthday: ' + json2.results.birthday + '**\n';
-										}
-										if (queries[q] === 'info' && g === 2) {
-											embedString += '**First Appearance: [' + json2.results.first_appeared_in_game.name + '](' + json2.results.first_appeared_in_game.site_detail_url + ')**';
-
-										}
-										if (queries[q] === 'info' && g === 5) {
-
-											if (json2.results.first_credited_game !== null) {
-												embedString += '**•First Credited Game: [' + json2.results.first_credited_game.name + '](' + json2.results.first_credited_game.site_detail_url + ')**\n';
-											}
-											var genders = ['0', 'Male', 'Female', '3'];
-											embedString += '**•Gender: ' + genders[json2.results.gender] + '**\n';
-											if (json2.results.birth_date !== null) {
-												embedString += '**•Birthday: ' + json2.results.birth_date + '**\n';
-											}
-										}
-
-										if (queries[q] === 'concepts') {
-											embedTitle += 'Concepts';
-										}
-										if (queries[q] === 'people') {
-											embedTitle += 'People';
-										}
-										if (queries[q] === 'locations') {
-											embedTitle += 'Locations';
-										}
-										if (queries[q] === 'objects') {
-											embedTitle += 'Objects';
-										}
-										if (g !== 1) {
-											if (queries[q] === 'characters') {
-												embedTitle += 'Characters';
-												for (var i = 0; i < json2.results.characters.length; i++) {
-													if ((embedString + '**•[' + json2.results.characters[i].name + '](' + json2.results.characters[i].site_detail_url + ')**\n').length < 2048)
-														embedString += '**•[' + json2.results.characters[i].name + '](' + json2.results.characters[i].site_detail_url + ')**\n';
-												}
-											}
-										} else {
-
-											if (queries[q] === 'friends') {
-												embedTitle += 'Friends';
-												for (var i = 0; i < json2.results.friends.length; i++) {
-													if ((embedString + '**•[' + json2.results.friends[i].name + '](' + json2.results.friends[i].site_detail_url + ')**\n').length < 2048)
-														embedString += '**•[' + json2.results.friends[i].name + '](' + json2.results.friends[i].site_detail_url + ')**\n';
-												}
-											}
-
-											if (queries[q] === 'enemies') {
-												embedTitle += 'Enemies';
-												for (var i = 0; i < json2.results.enemies.length; i++) {
-													if ((embedString + '**•[' + json2.results.enemies[i].name + '](' + json2.results.enemies[i].site_detail_url + ')**\n').length < 2048)
-														embedString += '**•[' + json2.results.enemies[i].name + '](' + json2.results.enemies[i].site_detail_url + ')**\n';
-												}
-											}
-										}
-										if (queries[q] === 'similar' && g === 0) {
-											embedTitle += 'Similar Games';
-											for (var i = 0; i < json2.results.similar_games.length; i++) {
-												if ((embedString + '**•[' + json2.results.similar_games[i].name + '](' + json2.results.similar_games[i].site_detail_url + ')**\n').length < 2048)
-													embedString += '**•[' + json2.results.similar_games[i].name + '](' + json2.results.similar_games[i].site_detail_url + ')**\n';
-											}
-										}
-										if (queries[q] === 'franchises' && g !== 3) {
-											embedTitle += 'Franchises';
-											for (var i = 0; i < json2.results.franchises.length; i++) {
-												if ((embedString + '**•[' + json2.results.franchises[i].name + '](' + json2.results.franchises[i].site_detail_url + ')**\n').length < 2048)
-													embedString += '**•[' + json2.results.franchises[i].name + '](' + json2.results.franchises[i].site_detail_url + ')**\n';
-											}
-										}
-
-										if (embedString.length > 2048) {
-											embedString = embedString.substring(0, 2048);
-										}
-										message.channel.send({
-											embed: {
-												title: embedTitle,
-												description: embedString,
-												url: json2.results.site_detail_url,
-												footer: {
-													text: 'From Giant Bomb Wiki'
-												},
-												color: 0xa81717,
-												thumbnail: {
-													url: imageURL
-												},
-											}
-										});
-									}
-								}
+							switch(g){
+								case 0:
+							gb.games.get(id, function (err2, res2, json2, 0) {
+							xtra.gbWiki(json2, query, message);	
 							});
+							break;
+							case 1:
+							gb.characters.get(id, function (err2, res2, json2, 1) {
+							xtra.gbWiki(json2, query, message);	
+							});
+							break;
+							case 2:
+							gb.concepts.get(id, function (err2, res2, json2, 2) {
+							xtra.gbWiki(json2, query, message);	
+							});
+							break;
+							case 3:
+							gb.franchises.get(id, function (err2, res2, json2, 3) {
+							xtra.gbWiki(json2, query, message);	
+							});
+							break;
+							case 4:
+							gb.companies.get(id, function (err2, res2, json2, 4) {
+							xtra.gbWiki(json2, query, message);	
+							});
+							break;
+							case 5:
+							gb.people.get(id, function (err2, res2, json2, 5) {
+							xtra.gbWiki(json2, query, message);	
+							});
+							break;
+							}
 							//message2.delete();
 							//message.delete();
 						}
--- xtra.js
+++ xtra.js
@@ -234,7 +234,7 @@ module.exports.bubble = (message) => {
 	
 }
 
-module.exports.gbwiki = (json, query) => {
+function gbwiki (json, query) {
 	var embedString = '';
 	if (query === 'locations'){
 		//embedTitle += 'Locations';
@@ -277,4 +277,230 @@ module.exports.getDirections = (result) => {
 				dir += (i + 1).toString() + '. ' + result.routes[0].legs[0].steps[i].html_instructions.replace(/<\/?b>/gm, '**').replace(/<div style="font-size:0.9em">/gm, ' `(').replace(/<\/div>/gm, ')`') + ' (' + result.routes[0].legs[0].steps[i].distance.text + ', ' + result.routes[0].legs[0].steps[i].duration.text + ')\n';
 			}
 			return dir + 'And you\'re there! :smiley:';	
+}
+
+module.exports.gbWiki = (json2, query, message, g) => {
+	var queries = query.split(',');
+								var Name = json2.results.name;
+								var imageURL = json2.results.image.original_url;
+								for (var q = 0; q < queries.length; q++) {
+									var embedTitle = Name + ' ';
+									var embedString = '';
+									embedString += gbwiki(json2, queries[q]);
+									//var embedImage = json2.results.image.original_url;
+									if (queries[q] === 'info' && g === 0) {
+										//console.log(json2.results.original_release_date);
+
+
+										var platforms = '';
+										var genres = '';
+										var themes = '';
+										var developers = '';
+										var publishers = ''
+											var dlcs = ''
+											for (var i = 0; i < json2.results.developers.length; i++) {
+
+												if ((developers + ', [' + json2.results.developers[i].name + '](' + json2.results.developers[i].site_detail_url + ')').length < 2048) {
+													if (i > 0) {
+														developers += ', ';
+													}
+													developers += '[' + json2.results.developers[i].name + '](' + json2.results.developers[i].site_detail_url + ')';
+												}
+											}
+											for (var i = 0; i < json2.results.publishers.length; i++) {
+												if ((publishers + ', [' + json2.results.publishers[i].name + '](' + json2.results.publishers[i].site_detail_url + ')').length < 2048) {
+													if (i > 0) {
+														publishers += ', ';
+													}
+													publishers += '[' + json2.results.publishers[i].name + '](' + json2.results.publishers[i].site_detail_url + ')';
+												}
+											}
+											if (json2.results.dlcs !== undefined && json2.results.dlcs !== null && typeof json2.results.dlcs !== undefined && typeof json2.results.dlcs !== null) {
+												for (var i = 0; i < json2.results.dlcs.length; i++) {
+
+													if ((dlcs + ', [' + json2.results.dlcs[i].name + '](' + json2.results.dlcs[i].site_detail_url + ')').length < 2048 && !dlcs.includes('[' + json2.results.dlcs[i].name + '](' + json2.results.dlcs[i].site_detail_url + ')')) {
+														if (i > 0) {
+															dlcs += ', ';
+														}
+														dlcs += '[' + json2.results.dlcs[i].name + '](' + json2.results.dlcs[i].site_detail_url + ')';
+													}
+												}
+											} else {
+												dlcs = 'None';
+											}
+											for (var i = 0; i < json2.results.platforms.length; i++) {
+
+												if ((platforms + ', [' + json2.results.platforms[i].name + '](' + json2.results.platforms[i].site_detail_url + ')').length < 2048) {
+													if (i > 0) {
+														platforms += ', ';
+													}
+													platforms += '[' + json2.results.platforms[i].name + '](' + json2.results.platforms[i].site_detail_url + ')';
+												}
+											}
+
+											for (var i = 0; i < json2.results.genres.length; i++) {
+
+												if ((genres + ', [' + json2.results.genres[i].name + '](' + json2.results.genres[i].site_detail_url + ')').length < 2048) {
+													if (i > 0) {
+														genres += ', ';
+													}
+													genres += '[' + json2.results.genres[i].name + '](' + json2.results.genres[i].site_detail_url + ')';
+												}
+											}
+											if (json2.results.themes !== undefined && json2.results.themes !== null && typeof json2.results.themes !== undefined && typeof json2.results.themes !== null) {
+												for (var i = 0; i < json2.results.themes.length; i++) {
+
+													if ((themes + '[' + json2.results.themes[i].name + '](' + json2.results.themes[i].site_detail_url + ')').length < 2048) {
+														if (i > 0) {
+															themes += ', ';
+														}
+														themes += '[' + json2.results.themes[i].name + '](' + json2.results.themes[i].site_detail_url + ')';
+													}
+												}
+											} else {
+												themes = 'None';
+											}
+
+											message.channel.send({
+												embed: {
+													title: embedTitle,
+													description: embedString,
+													url: json2.results.site_detail_url,
+													footer: {
+														text: 'From Giant Bomb Wiki'
+													},
+													color: 0xa81717,
+													thumbnail: {
+														url: imageURL
+													},
+													fields: [{
+															name: "Original Release Date",
+															value: json2.results.original_release_date.substring(0, json2.results.original_release_date.indexOf(' ')),
+															inline: true
+														}, {
+															name: "Platforms",
+															value: platforms,
+															inline: true
+														}, {
+															name: "Developers",
+															value: developers,
+															inline: true
+														}, {
+															name: "Publishers",
+															value: publishers,
+															inline: true
+														}, {
+															name: "Genres",
+															value: genres,
+															inline: true
+														}, {
+															name: "Themes",
+															value: themes,
+															inline: true
+														}, {
+															name: "DLCs",
+															value: dlcs
+														},
+													]
+												}
+											});
+
+									} else {
+										if (queries[q] === 'info' && g === 1) {
+
+											if (json2.results.first_appeared_in_game !== null) {
+												embedString += '**•First Appearance: [' + json2.results.first_appeared_in_game.name + '](' + json2.results.first_appeared_in_game.site_detail_url + ')**\n';
+											}
+											var genders = ['0', 'Male', 'Female', '3'];
+											embedString += '**•Gender: ' + genders[json2.results.gender] + '**\n';
+											embedString += '**•Birthday: ' + json2.results.birthday + '**\n';
+										}
+										if (queries[q] === 'info' && g === 2) {
+											embedString += '**First Appearance: [' + json2.results.first_appeared_in_game.name + '](' + json2.results.first_appeared_in_game.site_detail_url + ')**';
+
+										}
+										if (queries[q] === 'info' && g === 5) {
+
+											if (json2.results.first_credited_game !== null) {
+												embedString += '**•First Credited Game: [' + json2.results.first_credited_game.name + '](' + json2.results.first_credited_game.site_detail_url + ')**\n';
+											}
+											var genders = ['0', 'Male', 'Female', '3'];
+											embedString += '**•Gender: ' + genders[json2.results.gender] + '**\n';
+											if (json2.results.birth_date !== null) {
+												embedString += '**•Birthday: ' + json2.results.birth_date + '**\n';
+											}
+										}
+
+										if (queries[q] === 'concepts') {
+											embedTitle += 'Concepts';
+										}
+										if (queries[q] === 'people') {
+											embedTitle += 'People';
+										}
+										if (queries[q] === 'locations') {
+											embedTitle += 'Locations';
+										}
+										if (queries[q] === 'objects') {
+											embedTitle += 'Objects';
+										}
+										if (g !== 1) {
+											if (queries[q] === 'characters') {
+												embedTitle += 'Characters';
+												for (var i = 0; i < json2.results.characters.length; i++) {
+													if ((embedString + '**•[' + json2.results.characters[i].name + '](' + json2.results.characters[i].site_detail_url + ')**\n').length < 2048)
+														embedString += '**•[' + json2.results.characters[i].name + '](' + json2.results.characters[i].site_detail_url + ')**\n';
+												}
+											}
+										} else {
+
+											if (queries[q] === 'friends') {
+												embedTitle += 'Friends';
+												for (var i = 0; i < json2.results.friends.length; i++) {
+													if ((embedString + '**•[' + json2.results.friends[i].name + '](' + json2.results.friends[i].site_detail_url + ')**\n').length < 2048)
+														embedString += '**•[' + json2.results.friends[i].name + '](' + json2.results.friends[i].site_detail_url + ')**\n';
+												}
+											}
+
+											if (queries[q] === 'enemies') {
+												embedTitle += 'Enemies';
+												for (var i = 0; i < json2.results.enemies.length; i++) {
+													if ((embedString + '**•[' + json2.results.enemies[i].name + '](' + json2.results.enemies[i].site_detail_url + ')**\n').length < 2048)
+														embedString += '**•[' + json2.results.enemies[i].name + '](' + json2.results.enemies[i].site_detail_url + ')**\n';
+												}
+											}
+										}
+										if (queries[q] === 'similar' && g === 0) {
+											embedTitle += 'Similar Games';
+											for (var i = 0; i < json2.results.similar_games.length; i++) {
+												if ((embedString + '**•[' + json2.results.similar_games[i].name + '](' + json2.results.similar_games[i].site_detail_url + ')**\n').length < 2048)
+													embedString += '**•[' + json2.results.similar_games[i].name + '](' + json2.results.similar_games[i].site_detail_url + ')**\n';
+											}
+										}
+										if (queries[q] === 'franchises' && g !== 3) {
+											embedTitle += 'Franchises';
+											for (var i = 0; i < json2.results.franchises.length; i++) {
+												if ((embedString + '**•[' + json2.results.franchises[i].name + '](' + json2.results.franchises[i].site_detail_url + ')**\n').length < 2048)
+													embedString += '**•[' + json2.results.franchises[i].name + '](' + json2.results.franchises[i].site_detail_url + ')**\n';
+											}
+										}
+
+										if (embedString.length > 2048) {
+											embedString = embedString.substring(0, 2048);
+										}
+										message.channel.send({
+											embed: {
+												title: embedTitle,
+												description: embedString,
+												url: json2.results.site_detail_url,
+												footer: {
+													text: 'From Giant Bomb Wiki'
+												},
+												color: 0xa81717,
+												thumbnail: {
+													url: imageURL
+												},
+											}
+										});
+									}
+								}
 }
