86d5ccdc627a03745ad743f94d03152781fd1dfe
uulltt <ajvark@gmail.com>
2018-05-11 23:54:32 -0400

    making lines split sooner

--- bot.js
+++ bot.js
@@ -136,7 +136,7 @@ client.on('message', message => {
 	}
 	if (new RegExp(/[Ff]ont!crash\W/gm).test(message.content.substring(0, 11)) && message.content.length > 11){
 		var text = message.content.substring(11).toLowerCase().replace(/[^a-z0-9\.!\: ]/gm, '') + ' ';
-		var texts = text.match(/.{1,24}\W/gm);
+		var texts = text.match(/.{1,23}\W/gm);
 		for(var t = 0; t < texts.length; t++){
 			var paths = [];
 			var filename = texts[t] + '.png';
