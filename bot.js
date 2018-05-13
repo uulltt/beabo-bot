e04c9745834cd4ce2dd53ddbb9ff9d9cb67c875a
uulltt <ajvark@gmail.com>
2018-05-12 20:07:43 -0400

    bot.js stuff yeah

--- bot.js
+++ bot.js
@@ -252,7 +252,7 @@ client.on('message', message => {
 
 	}
 	if (message.content.startsWith('🍅 ') && message.content.length > 3){
-		if (!message.content.includes('coming soon') && !message.content.includes('box office'), !message.content.includes('opening')){
+		if (!message.content.includes('coming soon') && !message.content.includes('box office') && !message.content.includes('opening')){
 		var movieurl = 'https://www.rottentomatoes.com/m/' + encodeURI(message.content.substring(3).toLowerCase().replace(/ /gm, '_').replace(/[^a-z0-9_]/gm, ''));
 		rm.info(movieurl, function(err, info) {
 			rm.scores(movieurl, function(err2, scores) {
