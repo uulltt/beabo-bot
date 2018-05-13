d1dfe5b3fadd77096663c77b04813db6077c0c9d
uulltt <ajvark@gmail.com>
2018-05-12 23:49:09 -0400

    testing jpeg

--- bot.js
+++ bot.js
@@ -57,7 +57,8 @@ client.on('message', message => {
 		var request = require('request').defaults({
 				encoding: null
 			});
-		request.get(message.content.substring(2), function (err, res, body) {
+			console.log(message.content.substring(3));
+		request.get(message.content.substring(3), function (err, res, body) {
 			var exifString = ':frame_photo: EXIF data:\n';
 			try {
 				new ExifImage({
