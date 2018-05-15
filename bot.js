84e2271bc3e6cbb7a0b30efa9c415151a75bcb23
uulltt <ajvark@gmail.com>
2018-05-14 23:19:55 -0400

    fixing stuff

--- bot.js
+++ bot.js
@@ -240,7 +240,7 @@ client.on('message', message => {
 		}
 		if (beaboMessage.toLowerCase().substring(0, 9) === '!settime ' && beaboMessage.length > 9) {
 			var city = beaboMessage.substring(9);
-			herokupg.query('INSERT localtimes ( user_id, city_name) VALUES (\''+message.author.id+'\',\''+city+'\')ON CONFLICT (product_id) DO UPDATE notes = ‘arrived on time’;', (err, res) => {
+			herokupg.query('INSERT localtimes ( user_id, city_name) VALUES (\''+message.author.id+'\',\''+city+'\')ON CONFLICT (user_id) DO UPDATE city_name = \''+city+'\';', (err, res) => {
 				if (!err)
 				console.log(res);
 			else
@@ -258,7 +258,6 @@ client.on('message', message => {
 });
 		}
 		
-		if (beaboMessage.substring(0, 
 		if (beaboMessage.substring(0, 8) === '!numpad ' && beaboMessage.length > 8) {
 			var command = '**' + beaboMessage.substring(8) + '**';
 			command = command.replace(/1/gm, ':arrow_lower_left:').replace(/2/gm, ':arrow_down:').replace(/3/gm, ':arrow_lower_right:').replace(/4/gm, ':arrow_left:')
