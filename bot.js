441f1ca94ce1fd183b91fb41c5073a20be535ff2
uulltt <ajvark@gmail.com>
2018-05-15 16:51:30 -0400

    testing out blank canvas

--- bot.js
+++ bot.js
@@ -10,13 +10,15 @@ var pics = require('./pics.js');
 const movies = require('./rotten.js');
 const giantbomb = require('./gb.js');
 var EarthBoundText = require('./scripts/lib/ebtext.js');
-const { Client } = require('pg');
+const {
+	Client
+} = require('pg');
+var concat = require('concat-image');
 
 const herokupg = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
+		connectionString: process.env.DATABASE_URL,
+		ssl: true,
+	});
 
 client.on('ready', () => {
 	console.log('I am ready!');
@@ -36,7 +38,7 @@ function pad(n) {
 
 var lines = [" beabo", " bee", " bii", " be", " beeb"];
 
-function beeb () {
+function beeb() {
 	var len = Math.floor(Math.random() * 6) + 1;
 	var sentence = "";
 	for (var i = 0; i < len; i++) {
@@ -56,7 +58,7 @@ function beeb () {
 
 var cityTimezones = require('./citytime.js');
 var timezone = require('node-google-timezone');
-function cityTime (message) {
+function cityTime(message) {
 	var city = message.content.substring(7);
 	const citydata = cityTimezones.lookupViaCity(city);
 	try {
@@ -78,7 +80,7 @@ function cityTime (message) {
 
 }
 
-function localcityTime (message, city) {
+function localcityTime(message, city) {
 	const citydata = cityTimezones.lookupViaCity(city);
 	try {
 		var lati = citydata[city === 'London' ? 1 : 0].lat;
@@ -99,7 +101,7 @@ function localcityTime (message, city) {
 
 }
 
-function getDirections (result) {
+function getDirections(result) {
 	var dir = '';
 	for (var i = 0; i < result.routes[0].legs[0].steps.length; i++) {
 		dir += (i + 1).toString() + '. ' + result.routes[0].legs[0].steps[i].html_instructions.replace(/<\/?b>/gm, '**').replace(/<div style="font-size:0.9em">/gm, ' `(').replace(/<\/div>/gm, ')`') + ' (' + result.routes[0].legs[0].steps[i].distance.text + ', ' + result.routes[0].legs[0].steps[i].duration.text + ')\n';
@@ -107,46 +109,44 @@ function getDirections (result) {
 	return dir + 'And you\'re there! :smiley:';
 }
 
-function helpMessage (message) {
+function helpMessage(message) {
 	message.channel.send('http://uulltt.tumblr.com/beabo', {
-				embed: {
-					fields: [{
-							name: 'Font Commands',
-							value: 'To find a font name, go to https://nfggames.com/games/fontmaker/, select the game you want, right click the text and hit view image, and what\'s next to the "y-" in the url is your game.' +
-							'\nfont!gamename your text here - creates image of your text in the game\'s font\nb(u/d)(two digits)!game your text here to create a speech bubble going either up or down with the two digits determining the pointer position\nfont!game(two digits) your text here - that game with the first digit determining font style and second digit determining font size. also works for speech bubbles.\n'
-							 + 'Custom Fonts: font!crash, font!ms (metal slug), font!mario64, font!wario (warioware), font!puyo (Puyo Puyo), font!mk2 (mortal kombat 2)'
-						}, {
-							name: 'Google Maps Commands',
-							value: 'b!dir \"origin\" \"destination\" - prints directions from origin to destination\nb!places \"search query\" - finds places of a type near a location (e.g. \"arcades in miami\")\n'
-						}, {
-							name: 'Local Time Commands',
-							value: 'b!time cityname - gets local time of that city\nb!settime cityname - sets the local time for you based on the given city name\nb!gettime @user - fetches the local time for that user based on the city they set for themself\n'
-						}, {
-							name: 'Giant Bomb Wiki Commands',
-							value: 'b!gb game/character/company/etc. info/characters/concepts/locations/people/etc. name of thing - returns the info searched for relating to a game/character/company/etc.'
-						}, {
-							name: ':camera: Commands (:camera: or b!pics followed by)',
-							value: 'twitter, imgur, or tumblr album - posts the rest of the images from that album\na jpg image on the web - gets the EXIf data of that image\na link to a youtube vid - gets the thumbnail of that youtube vid'
-						}, {
-							name: 'Other Commands',
-							value: ':tomato: or b!rt movietitle - gets RottenTomatoes movie name, description, and critic/audience scores for a movie. type coming soon, opening, or box office instead of a movie title and it will bring up the top lists for those\nb!hex#hexCode - displays image of a color pertaining to the hex code' +
-							'\nb!numpad command - turns fighting game numpad notation into emoji\nb!list or b!todo your list here - separates a discord message into a list based on either line breaks or commas'
-						}
-					]
+		embed: {
+			fields: [{
+					name: 'Font Commands',
+					value: 'To find a font name, go to https://nfggames.com/games/fontmaker/, select the game you want, right click the text and hit view image, and what\'s next to the "y-" in the url is your game.' +
+					'\nfont!gamename your text here - creates image of your text in the game\'s font\nb(u/d)(two digits)!game your text here to create a speech bubble going either up or down with the two digits determining the pointer position\nfont!game(two digits) your text here - that game with the first digit determining font style and second digit determining font size. also works for speech bubbles.\n'
+					 + 'Custom Fonts: font!crash, font!ms (metal slug), font!mario64, font!wario (warioware), font!puyo (Puyo Puyo), font!mk2 (mortal kombat 2)'
+				}, {
+					name: 'Google Maps Commands',
+					value: 'b!dir \"origin\" \"destination\" - prints directions from origin to destination\nb!places \"search query\" - finds places of a type near a location (e.g. \"arcades in miami\")\n'
+				}, {
+					name: 'Local Time Commands',
+					value: 'b!time cityname - gets local time of that city\nb!settime cityname - sets the local time for you based on the given city name\nb!gettime @user - fetches the local time for that user based on the city they set for themself\n'
+				}, {
+					name: 'Giant Bomb Wiki Commands',
+					value: 'b!gb game/character/company/etc. info/characters/concepts/locations/people/etc. name of thing - returns the info searched for relating to a game/character/company/etc.'
+				}, {
+					name: ':camera: Commands (:camera: or b!pics followed by)',
+					value: 'twitter, imgur, or tumblr album - posts the rest of the images from that album\na jpg image on the web - gets the EXIf data of that image\na link to a youtube vid - gets the thumbnail of that youtube vid'
+				}, {
+					name: 'Other Commands',
+					value: ':tomato: or b!rt movietitle - gets RottenTomatoes movie name, description, and critic/audience scores for a movie. type coming soon, opening, or box office instead of a movie title and it will bring up the top lists for those\nb!hex#hexCode - displays image of a color pertaining to the hex code' +
+					'\nb!numpad command - turns fighting game numpad notation into emoji\nb!list or b!todo your list here - separates a discord message into a list based on either line breaks or commas'
 				}
-			});
+			]
+		}
+	});
 }
-
-
 
 const steamlink = 'https://store.steampowered.com/app/';
 const steamgames = ['514340', '514340', '514340', '658150', '658150', '522490', '598640'];
 const favegames = [steamlink + steamgames[0], steamlink + steamgames[1], steamlink + steamgames[2], steamlink + steamgames[3], steamlink + steamgames[4], steamlink + steamgames[5], steamlink + steamgames[6], 'https://dustinbragg.itch.io/yo-noid-was-ahead-of-its-time']
 client.on('message', message => {
 	if (message.isMentioned(client.user)) {
-		
-		if (message.content.toLowerCase().includes("help")){
-		helpMessage(message);
+
+		if (message.content.toLowerCase().includes("help")) {
+			helpMessage(message);
 		} else if (message.content.toLowerCase().match(/w(h?)(a|u)t('?)s (yo)?ur fav((e|orite)?) (steam|pc|computer|video|vidya)?( )?((ga([me]{2}))|vidya)(\?)?/gm)) {
 			message.channel.send(favegames[(Math.floor(Math.random() * (message.content.toLowerCase().includes("steam") ? 7 : 8)))] + '/');
 		} else {
@@ -179,7 +179,7 @@ client.on('message', message => {
 				for (var i = 0; i < Math.min(response.results.length, 5); i++) {
 					var open = '';
 					if (response.results[i].hasOwnProperty('opening_hours') && response.results[i].opening_hours.hasOwnProperty('open_now')) {
-							open = response.results[i].opening_hours.open_now ? ':large_blue_circle: ***OPEN NOW!***' : ':red_circle: Sorry, closed.';
+						open = response.results[i].opening_hours.open_now ? ':large_blue_circle: ***OPEN NOW!***' : ':red_circle: Sorry, closed.';
 					}
 					message.channel.send('**' + response.results[i].name + '**\n`' + response.results[i].formatted_address + '`\n:star: ' + response.results[i].rating + '\n' + open);
 				}
@@ -216,52 +216,76 @@ client.on('message', message => {
 		if (beaboMessage.substring(0, 4) === '!pg ' && message.author.id === process.env.BOT_ADMIN) {
 			herokupg.query(beaboMessage.substring(4), (err, res) => {
 				if (!err)
-				console.log(res);
-			else
-				console.log(err);
-});
+					console.log(res);
+				else
+					console.log(err);
+			});
 		}
 		if (beaboMessage.toLowerCase().substring(0, 9) === '!settime ' && beaboMessage.length > 9) {
 			var city = beaboMessage.substring(9);
-			console.log('INSERT INTO localtimes(user_id, city_name) VALUES (\''+message.author.id+'\',\''+city+'\')ON CONFLICT (user_id) DO UPDATE SET city_name = EXCLUDED.city_name;');
-			herokupg.query('INSERT INTO localtimes(user_id, city_name) VALUES (\''+message.author.id+'\',\''+city+'\')ON CONFLICT (user_id) DO UPDATE SET city_name = EXCLUDED.city_name;', (err, res) => {
-				if (!err){
-				console.log(res);
-				message.channel.send('Beabo bee! (Local time set for <@' + message.author.id + '>)');
-				}
-			else
-				console.log(err);
-});
+			console.log('INSERT INTO localtimes(user_id, city_name) VALUES (\'' + message.author.id + '\',\'' + city + '\')ON CONFLICT (user_id) DO UPDATE SET city_name = EXCLUDED.city_name;');
+			herokupg.query('INSERT INTO localtimes(user_id, city_name) VALUES (\'' + message.author.id + '\',\'' + city + '\')ON CONFLICT (user_id) DO UPDATE SET city_name = EXCLUDED.city_name;', (err, res) => {
+				if (!err) {
+					console.log(res);
+					message.channel.send('Beabo bee! (Local time set for <@' + message.author.id + '>)');
+				} else
+					console.log(err);
+			});
 		}
-		
+
 		if (beaboMessage.toLowerCase().substring(0, 11) === '!gettime <@' && beaboMessage.length > 9) {
 			var id = beaboMessage.substring(11).replace(/[^0-9]/gm, '');
 			herokupg.query('SELECT city_name FROM localtimes WHERE user_id = \'' + id + '\';', (err, res) => {
-				if (!err){
-				console.log(res);
-				localcityTime(message, res.rows[0].city_name);
-				}else
-				console.log(err);
-});
+				if (!err) {
+					console.log(res);
+					localcityTime(message, res.rows[0].city_name);
+				} else
+					console.log(err);
+			});
 		}
-		
-		if (beaboMessage.substring(0, 8) === '!numpad ' && beaboMessage.length > 8) {
-			var command = '**' + beaboMessage.substring(8) + '**';
-			command = command.replace(/1/gm, ':arrow_lower_left:').replace(/2/gm, ':arrow_down:').replace(/3/gm, ':arrow_lower_right:').replace(/4/gm, ':arrow_left:')
-				.replace(/7/gm, ':arrow_upper_left:').replace(/8/gm, ':arrow_up:').replace(/9/gm, ':arrow_upper_right:').replace(/6/gm, ':arrow_right:').replace(/5/gm, 'neutral');
-			message.channel.send(command);
-		}
-		if (beaboMessage.substring(0, 8) === '!ZiV-id ') {
-			message.channel.send('https://zenius-i-vanisher.com/v5.2/arcade.php?id=' + beaboMessage.substring(8) + '#summary');
-		}
-		
-		if (beaboMessage.substring(0, 11) === '!ZiV-random') {
-			message.channel.send('https://zenius-i-vanisher.com/v5.2/arcade.php?id=' + (Math.floor(Math.random() * 4000) + 2).toString() + '#summary');
-		}
-		if (beaboMessage.substring(0, 9) === '!commands' || beaboMessage.substring(0, 5) === '!help') {
-			helpMessage(message);
-		}
-	}
-});
 
-client.login(process.env.BOT_TOKEN);
+		if (new RegExp(/!eb(f|m|s)(n|m|s|b|p)\W/gm).test(beaboMessage.substring(0, 6))) {
+			const speedstring = 'fms'
+				const speeds = [15, 33, 50];
+			const flavors = ['plain', 'mint', 'strawberry', 'banana', 'peanut'];
+			const flavorstring = 'nmsbp'
+				var speed = speeds[speedstring.indexOf(beaboMessage.charAt(3))];
+			var flavor = flavors[flavorstring.indexof(beaboMessage.charAt(4)];
+			console.log(speed + ' ' + flavor);
+						var text = message.cleanContent.substring(7);
+
+						concat({
+							images: [fs.readFileSync('./eb-canvas.png')],
+							margin: 0 // optional, in px, defaults to 10px
+						}, function (err, canvas) {
+							message.channel.send({
+								files: [{
+										attachment: canvas.toBuffer(),
+										name: 'crash.png'
+									}
+								]
+							});
+						});
+
+			}
+
+			if (beaboMessage.substring(0, 8) === '!numpad ' && beaboMessage.length > 8) {
+				var command = '**' + beaboMessage.substring(8) + '**';
+				command = command.replace(/1/gm, ':arrow_lower_left:').replace(/2/gm, ':arrow_down:').replace(/3/gm, ':arrow_lower_right:').replace(/4/gm, ':arrow_left:')
+					.replace(/7/gm, ':arrow_upper_left:').replace(/8/gm, ':arrow_up:').replace(/9/gm, ':arrow_upper_right:').replace(/6/gm, ':arrow_right:').replace(/5/gm, 'neutral');
+				message.channel.send(command);
+			}
+			if (beaboMessage.substring(0, 8) === '!ZiV-id ') {
+				message.channel.send('https://zenius-i-vanisher.com/v5.2/arcade.php?id=' + beaboMessage.substring(8) + '#summary');
+			}
+
+			if (beaboMessage.substring(0, 11) === '!ZiV-random') {
+				message.channel.send('https://zenius-i-vanisher.com/v5.2/arcade.php?id=' + (Math.floor(Math.random() * 4000) + 2).toString() + '#summary');
+			}
+			if (beaboMessage.substring(0, 9) === '!commands' || beaboMessage.substring(0, 5) === '!help') {
+				helpMessage(message);
+			}
+		}
+	});
+
+	client.login(process.env.BOT_TOKEN);
