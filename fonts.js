ff12a14e15584f62411aaea78a8ec8ad7fe3b711
uulltt <ajvark@gmail.com>
2018-05-15 23:42:35 -0400

    man i keep noticing potential bugs and i haven't even started testing yet

--- fonts.js
+++ fonts.js
@@ -347,10 +347,14 @@ files: [{
 			texts[t] = texts[t].replace(/\n/gm, '');
 			var cursor = 0;
 for(;cursor < texts[t].length; cursor++){
+	if (texts[t].charCodeAt(cursor) > 31 && texts[t].charCodeAt(cursor) < 96){
 	if (texts[t].charCodeAt(cursor) < 100)
 	paths[cursor] = fs.readFileSync('./doom/STCFN0' + (texts[t].charCodeAt(cursor)).toString() + '.png'),
 else
 	paths[cursor] = fs.readFileSync('./doom/STCFN' + (texts[t].charCodeAt(cursor)).toString() + '.png'),
+	} else {
+		paths[cursor] = fs.readFileSync('./doom/STCFN032.png'),
+	}
 }
 if (cursor === texts[t].length){
 	concat({
