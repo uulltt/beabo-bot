a01cda9ddc388da06c16df6c19a43af12c12ad50
uulltt <ajvark@gmail.com>
2018-06-15 15:19:52 -0400

    idk why the rect is always black

--- bot.js
+++ bot.js
@@ -261,14 +261,14 @@ var word = beaboMessage.substring(beaboMessage.indexOf(' ') + 1).replace(/\W/gm,
 			var word = beaboMessage.substring(beaboMessage.indexOf(" ")).trim();
 			var textCanvas = new Canvas.createCanvas(120 * word.length, 276);
 			var ctx = textCanvas.getContext("2d");
-			ctx.fillstyle = "red";
+			ctx.fillstyle = 'red';
 			console.log(ctx.fillStyle);
 			ctx.fillRect(0, 0, 120*word.length, 276);
 			//ctx.fill();
 			ctx.font = '150px "Supreme"';
 			ctx.fillStyle = "white";
 			ctx.textAlign="center"; 
-			ctx.fillText(word, 60 * word.length, 276);
+			ctx.fillText(word, 60 * word.length, 207);
 			message.channel.send({
 						files: [{attachment: textCanvas.toBuffer(),name: 'supreme.png'}]
 					});
