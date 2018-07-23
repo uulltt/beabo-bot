b70541c69fe98910a4988f5a140195d0f77f32b5
uulltt <ajvark@gmail.com>
2018-07-22 21:42:37 -0400

    maybe this will work

--- bot.js
+++ bot.js
@@ -543,7 +543,7 @@ client.on('message', message => {
 					
 					client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find('name', 'member-log');
+  const channel = member.guild.channels.find('name', 'general');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
