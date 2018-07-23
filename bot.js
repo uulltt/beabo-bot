27246815af2d6448e1ebff91a206dcf08137e0fb
uulltt <ajvark@gmail.com>
2018-07-22 22:47:09 -0400

    need to use * instead of +

--- bot.js
+++ bot.js
@@ -124,7 +124,7 @@ const steamgames = ['514340', '514340', '514340', '658150', '658150', '522490', 
 const favegames = [steamlink + steamgames[0], steamlink + steamgames[1], steamlink + steamgames[2], steamlink + steamgames[3], steamlink + steamgames[4], steamlink + steamgames[5], steamlink + steamgames[6], 'https://dustinbragg.itch.io/yo-noid-was-ahead-of-its-time']
 const webcomics = ['http://dreamrise-comic.com', 'http://endlesshallscomic.tumblr.com', 'http://www.monster-lands.com'];
 client.on('message', message => {
-	if ((message.content.toLowerCase().includes('@y\'all') || message.content.toLowerCase().includes('@yall')) && !(new RegExp(/`[^`]+@y('?)all[^`]+`/gm)).test(message.content.toLowerCase())){
+	if ((message.content.toLowerCase().includes('@y\'all') || message.content.toLowerCase().includes('@yall')) && !(new RegExp(/`[^`]*@y('?)all[^`]*`/gm)).test(message.content.toLowerCase())){
 		message.channel.fetchMessages({ limit: 15 })
   .then(messages => message.channel.send(messages.array().map(function(item){
 		return '<@' + item.author.id  + '>';
