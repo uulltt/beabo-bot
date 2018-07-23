0d0001f57558038c7984e0e9619aa374c24b9c95
uulltt <ajvark@gmail.com>
2018-07-22 21:53:44 -0400

    are we really getting @y'all

--- bot.js
+++ bot.js
@@ -124,6 +124,15 @@ const steamgames = ['514340', '514340', '514340', '658150', '658150', '522490', 
 const favegames = [steamlink + steamgames[0], steamlink + steamgames[1], steamlink + steamgames[2], steamlink + steamgames[3], steamlink + steamgames[4], steamlink + steamgames[5], steamlink + steamgames[6], 'https://dustinbragg.itch.io/yo-noid-was-ahead-of-its-time']
 const webcomics = ['http://dreamrise-comic.com', 'http://endlesshallscomic.tumblr.com', 'http://www.monster-lands.com'];
 client.on('message', message => {
+	if (message.content.includes('@y\'all'){
+		message.channel.fetchMessages({ limit: 50 })
+  .then(messages => message.channel.send(messages.array().map(function(item){
+		return '<@' + item.author.id  + '>';
+	  }).toString();
+	  ))
+  .catch(console.error);
+	}
+	
 	if (message.content.includes('@everyone')){
 		var chance = Math.floor(Math.random() * 100);
 		if (chance === 0){
