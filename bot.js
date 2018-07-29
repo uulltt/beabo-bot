5c6d0d2dcdaba512f9804d0cdba024f298de90c9
uulltt <ajvark@gmail.com>
2018-07-29 00:26:42 -0400

    adding voice stuff back

--- bot.js
+++ bot.js
@@ -191,8 +191,9 @@ client.on('message', async message => {
 		if (message.isMentioned(client.user) && !message.cleanContent.toLowerCase().includes('🖕') && !(message.cleanContent.toLowerCase().match(/(not (cu|valid))|(do( ?)n(('|o)?)t l(o|u)v)/gm) && (message.cleanContent.toLowerCase().match(/((l(o|u)v(e?))|(<3)) (((yo)?)u|(ya(h?)))/gm) || message.cleanContent.toLowerCase().match(/c(u+)te/gm) || message.cleanContent.toLowerCase().includes("best") || message.cleanContent.toLowerCase().includes("valid"))) && !(message.cleanContent.toLowerCase().match(/((f(u|(ri))([ck]{1,2}))|(hate)) (yo)?u/gm) || message.cleanContent.toLowerCase().match(/(yo)?u su([ck]{1,2})/gm))) {
 
 			if (!message.cleanContent.toLowerCase().match(/(not (cu|valid))|(do( ?)n(('|o)?)t l(o|u)v)/gm) && (message.cleanContent.toLowerCase().match(/((l(o|u)v(e?))|(<3)) (((yo)?)u|(ya(h?)))/gm) || message.cleanContent.toLowerCase().match(/c(u+)te/gm) || message.cleanContent.toLowerCase().includes("best") || message.cleanContent.toLowerCase().includes("valid"))) {
-				
-				/*if (message.member.voiceChannel) {
+				if (message.channel.hasOwnProperty('guild') && message.member.voiceChannel) {
+									herokupg.query("SELECT voice FROM permissions WHERE guild_id = \'" + message.guild.id.toString() + "\';", async function(err, res) {
+										if (res.rows[0].voice){
       const connection = await message.member.voiceChannel.join();
 	  const dispatcher = connection.playFile('./beabo_'+(Math.floor(Math.random() * (7)))+'.mp3');
 	  dispatcher.setVolume(0.2); // half the volume
@@ -200,9 +201,14 @@ client.on('message', async message => {
 dispatcher.on('end', () => {
   message.guild.voiceConnection.channel.leave();
 });
-    } else {*/
+										} else {
+											message.channel.send("bee bee biiiii! :heart:");
+										}
+									});
+    } else {
 		message.channel.send("bee bee biiiii! :heart:");
-	//}
+	}
+				
 			}
 				else if (message.content.toLowerCase().includes("help")) {
 					helpMessage(message);
@@ -249,16 +255,6 @@ dispatcher.on('end', () => {
 					pics(message, message.content);
 					if (message.content.toLowerCase().charAt(0) === 'b') {
 						var beaboMessage = message.content.substring(1);
-
-						if (new RegExp(/!hex#[0-9A-Fa-f]{6}/gm).test(beaboMessage.substring(0, 11))) {
-							message.channel.send({
-								embed: {
-									image: {
-										url: 'https://www.colorcombos.com/images/colors/' + beaboMessage.substring(5, 11) + '.png'
-									}
-								}
-							});
-						}
 
 						if (beaboMessage.substring(0, 8) === '!places ') {
 							parameters = {
@@ -504,7 +500,9 @@ dispatcher.on('end', () => {
 										}
 									]
 								});
- /*if (message.member.voiceChannel) {
+ if (message.channel.hasOwnProperty('guild') && message.member.voiceChannel) {
+									herokupg.query("SELECT voice FROM permissions WHERE guild_id = \'" + message.guild.id.toString() + "\';", async function(err, res) {
+										if (res.rows[0].voice){
       const connection = await message.member.voiceChannel.join();
 	  const dispatcher = connection.playFile('./sunny.mp3');
 	  dispatcher.setVolume(0.2); // half the volume
@@ -512,7 +510,9 @@ dispatcher.on('end', () => {
 dispatcher.on('end', () => {
   message.guild.voiceConnection.channel.leave();
 });
-    }*/
+										}
+									});
+ }
 	
 								
 							}
