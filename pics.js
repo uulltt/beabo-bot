f0b2d93d5337981bf63a330dcb90d7c1acbbba4e
uulltt <ajvark@gmail.com>
2018-08-26 19:55:11 -0400

    trying to figure out something

--- pics.js
+++ pics.js
@@ -150,6 +150,7 @@ module.exports = (message, content, herokupg) => {
 						if (error)
 							message.channel.send('Error: ' + error.message);
 						else {
+							console.log(exifData);
 							var propValue;
 							const propholders = [exifData.image, exifData.thumbnail, exifData.exif, exifData.gps, exifData.interoperability];
 							for (var i = 0; i < 5; i++) {
