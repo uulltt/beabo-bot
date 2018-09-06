d84666fcc74f49c117ba631c14141acfa76db931
uulltt <ajvark@gmail.com>
2018-09-06 17:51:37 -0400

    testing out making the midi

--- markov-midi.js
+++ markov-midi.js
@@ -16,4 +16,19 @@ console.log(JSON.stringify(Player.getEvents()));
 							
 							});
 	}
+	if (content.startswith('b!makemidi')){
+		var track = new MidiWriter.Track();
+ 
+// Define an instrument (optional):
+track.addEvent(new MidiWriter.ProgramChangeEvent({instrument : 1}));
+ 
+// Add some notes:
+var note = new MidiWriter.NoteEvent({pitch:['C4', 'D4', 'E4'], duration: '4'});
+track.addEvent(note);
+ 
+// Generate a data URI
+var write = new MidiWriter.Writer([track]);
+console.log(write.buildFile);
+		
+	}
 }
