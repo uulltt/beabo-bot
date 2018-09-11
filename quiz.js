00928e9412dd87da134da64ebc5eae33f54f64ac
uulltt <ajvark@gmail.com>
2018-09-10 22:02:01 -0400

    fixing various issues

--- quiz.js
+++ quiz.js
@@ -9,7 +9,15 @@ const request = require('request').defaults({
 		var quizID = content.match(/\/[0-9]+\//gm)[0].match(/[0-9]+/gm)[0];
 		request.get('https://api.quizlet.com/2.0/sets/'+quizID+'?client_id='+clientID+'&whitespace=1', function(err, res, body){
 			var quiz = JSON.parse(body.toString());
-			var q = quiz.terms[Math.floor(Math.random() * quiz.terms.length)];
+			testQuestion(message, content, quiz, 0, 1)
+		});
+		}
+		
+		
+	}
+	
+	function testQuestion(message, content, quiz, i, len){
+		var q = quiz.terms[Math.floor(Math.random() * quiz.terms.length)];
 			var question = q.term;
 			var answer = q.definition.toLowerCase().replace(/[^ 0-9a-z]/gm, '');
 			message.channel.send('\"' + question + '\"');
@@ -21,15 +29,13 @@ const request = require('request').defaults({
 							var a = message2.cleanContent.toLowerCase().replace(/[^ 0-9a-z]/gm, '');
 							if (a == answer){
 								message2.react('✅');
+								collector.stop();
 							} else {
 								message2.react('❎');
 								message.channel.send('\"' + q.definition + '\"');
+								collector.stop();
 							}
 						}
 
 					});
-		});
-		}
-		
-		
 	}
