41086192412fea7118216fac8ba77a409314c9de
uulltt <ajvark@gmail.com>
2018-09-11 21:25:44 -0400

    might get the music bot working this time

--- music.js
+++ music.js
@@ -9,29 +9,80 @@ const request = require('request').defaults({
 var tumblr = new Tumblr({
 		consumerKey: process.env.TUMBLR_CONSUMER_KEY,
 	});
-	
-module.exports = async function(client, message, content, herokupg) {
-if (content.startsWith('b!play') && message.member.voiceChannel){
-	var link = content.substring(6).trim();
-	console.log(link);
-	const connection = await message.member.voiceChannel.join();
-	if (link.includes('youtube.com/watch?v=') || link.includes('youtu.be/')){
-		connection.play(ytdl(
-  link,
-  { filter: 'audioonly' }));
 
-	}
-	else if (link.includes('soundcloud.com/')){
-		soundcloudDl.getSongDlByURL(link).then(function(song){
-		console.log(song);
-    connection.play(song.http_mp3_128_url);
-});
-	}
-	else if (link.match(/\/post\/[0-9]+/gm)){
-		
-	} else {
-		message.channel.send('BEABOOOO! (Error. This cannot be played currently.)')
-	}
-}
+function play(connection, message) {
+	var server = servers[message.guild.id];
+	var link = server.queue[0];
+	if (link.includes('youtube.com/watch?v=') || link.includes('youtu.be/')) {
+		server.dispatcher = connection.playStream(ytdl(
+					link, {
+					filter: 'audioonly'
+				}));
+		server.queue.shift();
+		server.dispatcher.on('end', function () {
+			if (server.queue[0]) {
+				play(connection, message);
+			} else {
+				connection.disconnect();
+			}
+		}
+		} else if (link.includes('soundcloud.com/')) {
+			soundcloudDl.getSongDlByURL(link).then(function (song) {
+				console.log(song);
+				server.dispatcher = connection.playStream(song.http_mp3_128_url);
+				
+			} else if (link.match(/\/post\/[0-9]+/gm)) {
+				var blogId = content.substring(content.indexOf('://') + 3, content.indexOf('/post/'));
+				var postId = parseInt(content.substring(content.indexOf('/post/') + 6).match(/[0-9]+/gm)[0]);
+				tumblr.get('/posts', {
+					hostname: blogId,
+					id: postId
+				}, function (err, json) {
+					if (json.total_posts > 0 && json.posts[0].type === 'audio') {
+						var r = request.get(json.posts[0].audio_source_url, function (err, res, body) {
+								server.dispatcher = connection.playStream(r.uri.href);
+								server.queue.shift();
+								server.dispatcher.on('end', function () {
+									if (server.queue[0]) {
+										play(connection, message);
+									} else {
+										connection.disconnect();
+									}
+								}
+								});
+						}
+					});
+				} else {
+					message.channel.send('BEABOOOO! (Error. This cannot be played currently.)');
+					server.queue.shift();
+					if (server.queue[0]) {
+						play(connection, message);
+					} else {
+						connection.disconnect();
+					}
+				}
 
-}
+				
+	});
+
+			}
+
+			module.exports = async function (client, message, content, herokupg) {
+				if (content.startsWith('b!play') && message.member.voiceChannel) {
+					var link = content.substring(6).trim();
+					console.log(link);
+					if (!servers[message.guild.id]) {
+						servers[message.guild.id] = {
+							queue: []
+						};
+					}
+					queue[queue.length] = link;
+					const connection = await message.member.voiceChannel.join();
+					play(connection, message);
+				}
+				if (content.startsWith('b!skip') && message.member.voiceChannel){
+				var server = servers[message.guild.id];
+				if (server.dispatcher) server.dispatcher.end();
+				}
+
+			}
