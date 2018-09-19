59a8453b122a555ed27df66170eae9367454ad4b
uulltt <ajvark@gmail.com>
2018-09-18 22:31:32 -0400

    trying to get sheeting functionality for Beabo

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ var Image = Canvas.Image;
 var Font = Canvas.Font;
 var path = require('path');
 var quiz = require('./quiz.js');
-
+var gifyParse = require('gify-parse');
 Canvas.registerFont('./fonts/COOPBL.TTF', {
 	family: 'Cooper Black'
 	});
@@ -320,7 +320,14 @@ client.on('message', async message => {
 		
 	if (message.content.toLowerCase().startsWith('b!')) {
 		var beaboMessage = message.content.substring(2);
-
+if (beaboMessage.startsWith('gif')){
+	if (message.attachments.array().length > 0 && message.attachments.array()[0].url.includes('.gif')){
+		request.get(message.attachments.array()[0].url, function (err, res, body) {
+			console.log(gifyParse.getInfo(body));
+	});
+	}
+	
+}
 		/*if (beaboMessage.substring(0, 4) === '!gb ') {
 		giantbomb(message, beaboMessage);
 		}*/
