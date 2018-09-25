5a87ca4054aac63dfb7bcd34115e4dedde94e648
uulltt <ajvark@gmail.com>
2018-09-24 20:03:13 -0400

    might need to take this option

--- bot.js
+++ bot.js
@@ -366,9 +366,9 @@ client.on('message', async message => {
 		var img2 = new Image;
 			img2.src = canvas.toBuffer();
 			var ctx = canvas.getContext('2d');
-			ctx.drawImage(img, 0, 232, 669, 640);
+			ctx.drawImage(img, 0, 224, 669, 648);
 			ctx.drawImage(img2, 0, 0, 669, 960);
-			ctx = canvas.getContext("2d", {alpha: false})
+			canvas.imageSmoothingEnabled = false;
 			ctx.font = '60px "GooseBumps"';
 			ctx.fillStyle = "white";
 			ctx.textAlign = "center";
