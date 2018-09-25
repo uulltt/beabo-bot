bcc053eb799146fe83153c24061989c3232ad65f
uulltt <ajvark@gmail.com>
2018-09-24 20:04:01 -0400

    fixing beabo again

--- bot.js
+++ bot.js
@@ -366,7 +366,7 @@ client.on('message', async message => {
 		var img2 = new Image;
 			img2.src = canvas.toBuffer();
 			var ctx = canvas.getContext('2d');
-			ctx.drawImage(img, 0, 224, 669, 648);
+			ctx.drawImage(img, 0, 240, 669, 640);
 			ctx.drawImage(img2, 0, 0, 669, 960);
 			canvas.imageSmoothingEnabled = false;
 			ctx.font = '60px "GooseBumps"';
