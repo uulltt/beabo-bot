e4a55829956bcca793be17f0f034b8a8c2041359
uulltt <ajvark@gmail.com>
2018-09-25 21:13:16 -0400

    moving text boxes into their own js file

--- bot.js
+++ bot.js
@@ -1,36 +1,15 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var fs = require('fs');
 var fonts = require('./fonts.js');
 var music = require('./music.js');
 var pics = require('./pics.js');
-var Canvas = require('canvas');
-var concat = require('./concat.js');
-var Image = Canvas.Image;
-var Font = Canvas.Font;
-var path = require('path');
 var quiz = require('./quiz.js');
-Canvas.registerFont('./fonts/COOPBL.TTF', {
-	family: 'Cooper Black'
-});
-Canvas.registerFont('./fonts/Futura Std Heavy Oblique.otf', {
-	family: 'Supreme'
-});
-
-Canvas.registerFont('./fonts/Futura Condensed Medium.otf', {
-	family: 'GooseBumps'
-});
-Canvas.registerFont('./fonts/Textile.ttf', {
-	family: 'Textile'
-});
-Canvas.registerFont('./fonts/Korinna Bold.ttf', {
-	family: 'Jeopardy'
-});
+var textboxes = require('./textboxes.js');
 var request = require('request').defaults({
 		encoding: null
 	});
-var EarthBoundText = require('./scripts/lib/ebtext.js');
+
 var timestuff = require('./timestuff.js');
 const {
 	Client
@@ -385,7 +364,7 @@ client.on('message', async message => {
 		}
 
 		timestuff(message, beaboMessage, herokupg);
-
+		textboxes(message, beaboMessage);
 		/*if (beaboMessage.toLowerCase().startsWith("nirvanna ")) {
 			var word = message.cleanContent.substring(message.cleanContent.indexOf(" ")).toLowerCase().trim();
 			var textCanvas = new Canvas.createCanvas(600, 300);
@@ -409,199 +388,7 @@ client.on('message', async message => {
 			});
 		}*/
 
-		if (beaboMessage.toLowerCase().startsWith("jeopardy ")) {
-			var word = message.cleanContent.substring(message.cleanContent.indexOf(" ")).trim().toUpperCase() + '\u200B';
-			var textCanvas = new Canvas.createCanvas(1280, 720);
-			var ctx = textCanvas.getContext("2d");
-			ctx.fillStyle = "blue";
-			ctx.rect(0, 0, 1280, 720);
-			ctx.fill();
-			ctx.font = '80px "Jeopardy"';
-			ctx.fillStyle = "white";
-			ctx.textAlign = "center";
-			ctx.textBaseline = "middle";
-
-			ctx.shadowColor = "black";
-			ctx.shadowOffsetX = 10; // integer
-			ctx.shadowOffsetY = 10; // integer
-			ctx.shadowBlur = 10; // integer
-			var words = word.match(/.{1,22}\W/gm);
-
-			for (var i = 0; i < words.length; ctx.fillText(words[i], 640, (i * 120) - ((words.length - 1) * 60) + 360), i++);
-
-			message.channel.send({
-				files: [{
-						attachment: textCanvas.toBuffer(),
-						name: 'jeopardy.png'
-					}
-				]
-			});
-		}
-
-		if (beaboMessage.toLowerCase().startsWith("sunny ") || beaboMessage.toLowerCase().startsWith("iasip ")) {
-			var word = '\"' + message.cleanContent.substring(message.cleanContent.indexOf(" ")).trim() + '\"';
-			var textCanvas = new Canvas.createCanvas(1280, 720);
-			var ctx = textCanvas.getContext("2d");
-			ctx.fillStyle = "black";
-			ctx.rect(0, 0, 1280, 720);
-			ctx.fill();
-			ctx.font = '60px "Textile"';
-			ctx.fillStyle = "white";
-			ctx.textAlign = "center";
-			ctx.textBaseline = "middle";
-			var words = word.match(/.{1,36}\W/gm);
-
-			for (var i = 0; i < words.length; ctx.fillText(words[i], 640, (i * 120) - ((words.length - 1) * 60) + 360), i++);
-			message.channel.send({
-				files: [{
-						attachment: textCanvas.toBuffer(),
-						name: 'sunny.png'
-					}
-				]
-			});
-			if (message.channel.hasOwnProperty('guild') && message.member.voiceChannel && message.guild.voiceConnection == null) {
-				herokupg.query("SELECT voice FROM permissions WHERE guild_id = \'" + message.guild.id.toString() + "\';", async function (err, res) {
-					if (res.rows[0].voice) {
-						const connection = await message.member.voiceChannel.join();
-						const dispatcher = connection.playFile('./sunny.mp3');
-						dispatcher.setVolume(0.2); // half the volume
-
-						dispatcher.on('end', () => {
-							message.guild.voiceConnection.channel.leave();
-						});
-					}
-				});
-			}
-
-		}
-
-		if (beaboMessage.toLowerCase().startsWith("supreme ")) {
-			var word = message.cleanContent.substring(message.cleanContent.indexOf(" ")).trim();
-			var textCanvas = new Canvas.createCanvas((110 * word.length), 220);
-			var ctx = textCanvas.getContext("2d");
-			ctx.fillStyle = '#DA2727';
-			ctx.fillRect(0, 0, (110 * word.length), 220);
-			//ctx.fill();
-			ctx.font = '180px "Supreme"';
-			ctx.fillStyle = "white";
-			ctx.textAlign = "left";
-			ctx.fillText(word, 55, 160);
-			message.channel.send({
-				files: [{
-						attachment: textCanvas.toBuffer(),
-						name: 'supreme.png'
-					}
-				]
-			});
-		}
 		
-		if (new RegExp(/b!goosebumps/gm).test(message.content.toLowerCase().substring(0, 12)) && message.attachments.array().length > 0 && message.attachments.array()[0].width > 0){
-		request.get(message.attachments.array()[0].url, function(err, res, body){
-		var img = new Image;
-		img.src = body;
-		
-		concat({
-			images: [fs.readFileSync('./goosebumps'+Math.floor(Math.random() * 3).toString()+'.png')], margin: 0
-		}, function(err, canvas){
-		var img2 = new Image;
-			img2.src = canvas.toBuffer();
-			var ctx = canvas.getContext('2d');
-			ctx.drawImage(img, 0, 240, 669, 640);
-			ctx.drawImage(img2, 0, 0, 669, 960);
-			canvas.imageSmoothingEnabled = false;
-			ctx.font = '60px "GooseBumps"';
-			ctx.fillStyle = "white";
-			ctx.textAlign = "center";
-			ctx.textBaseline = "Alphabetic";
-			if (message.content.length > 13){
-			var words = message.content.substring(13).trim().split('\n');
-			words[0] = words[0].toUpperCase()
-			ctx.fillText(words[0], 335, 910);
-			ctx.fillStyle = "black"
-			ctx.fillText(message.member.displayName.toUpperCase(), 335, 75);
-			ctx.font = '24px "GooseBumps"';
-			
-			var xOffset = Math.floor((Math.random() * 580)) + 20;
-			var yOffset = Math.floor((Math.random() * 500)) + 300;
-			for(var i = 1; i < words.length; i++){
-			ctx.fillText(words[i], xOffset + 2, yOffset + (i * 24) + 2);
-			}
-			ctx.fillStyle = "white";
-			for(var i = 1; i < words.length; i++){
-			ctx.fillText(words[i], xOffset, yOffset + (i * 24));
-			}
-			}
-			message.channel.send({
-						files: [{attachment: canvas.toBuffer(),name: 'goosebumps.png'}]
-					});
-		});
-		});
-		}
-
-		if (new RegExp(/eb(n|m|s|b|p|N|M|S|B|P)\W/gm).test(beaboMessage.substring(0, 4))) {
-			const flavors = ['plain', 'mint', 'strawberry', 'banana', 'peanut'];
-			const flavorstring = 'nmsbp'
-				var Flavor = flavors[flavorstring.indexOf(beaboMessage.charAt(3).toLowerCase())];
-			var Text = message.cleanContent.substring(6);
-			var textCanvas = new Canvas.createCanvas(608, 256);
-			textCanvas.imageSmoothingEnabled = false;
-			EarthBoundText.preload_assets();
-			EarthBoundText.render({
-				canvas: textCanvas,
-				flavor: Flavor,
-				text: Text,
-				Message: message
-			});
-		}
-
-		if (new RegExp(/eb\W/gm).test(beaboMessage.substring(0, 3))) {
-			var Text = message.cleanContent.substring(5);
-			var textCanvas = new Canvas.createCanvas(608, 256);
-			textCanvas.imageSmoothingEnabled = false;
-			EarthBoundText.preload_assets();
-			EarthBoundText.render({
-				canvas: textCanvas,
-				flavor: 'plain',
-				text: Text,
-				Message: message
-			});
-		}
-
-		if (new RegExp(/ut\W/gm).test(beaboMessage.substring(0, 3))) {
-			var text = message.cleanContent.substring(5);
-			message.channel.send({
-				embed: {
-					image: {
-						url: 'https://www.demirramon.com/gen/undertale_box.png?text=' + encodeURI(text)
-					}
-				}
-			});
-		} else {
-			if ((beaboMessage).match(/ut_.+\W/gm)) {
-				var characterText = message.cleanContent.substring(message.cleanContent.indexOf('t') + 2);
-				var characterexp = characterText.substring(0, characterText.search(/\W/gm)).split('_');
-				var character = characterexp[0];
-				var text = characterText.substring(characterText.search(/\W/gm) + 1);
-				if (characterexp.length <= 1) {
-					message.channel.send({
-						embed: {
-							image: {
-								url: 'https://www.demirramon.com/gen/undertale_box.png?character=' + encodeURI(character) + '&text=' + encodeURI(text)
-							}
-						}
-					});
-				} else {
-					var expression = characterexp[1];
-					message.channel.send({
-						embed: {
-							image: {
-								url: 'https://www.demirramon.com/gen/undertale_box.png?character=' + encodeURI(character) + '&expression=' + encodeURI(expression) + '&text=' + encodeURI(text)
-							}
-						}
-					});
-				}
-			}
-		}
 
 		if (beaboMessage.substring(0, 8) === 'commands' || beaboMessage.substring(0, 4) === 'help') {
 			helpMessage(message);
