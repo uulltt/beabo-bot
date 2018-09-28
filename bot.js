134bee6735ad73c33ab3f64c21df291b488af67a
uulltt <ajvark@gmail.com>
2018-09-28 13:44:41 -0400

    trying to figure something out

--- bot.js
+++ bot.js
@@ -339,7 +339,7 @@ client.on('message', async message => {
 		request.get(message.attachments.array()[0].url, function(err, res, body){
 		Tesseract.recognize(body)
          .progress(function  (p) { console.log('progress', p)    })
-         .then(function (result) { console.log('result', result) })
+         .then(function (result) { message.channel.send(JSON.stringify(result).substring(0, 2000))})
 		});
 		
 		}
