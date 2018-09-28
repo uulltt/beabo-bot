596fb250cae1e1f6fb8b0eb51af9b8f13bc5a5a3
uulltt <ajvark@gmail.com>
2018-09-28 18:56:10 -0400

    whoops

--- bot.js
+++ bot.js
@@ -10,9 +10,6 @@ var Tesseract = require('tesseract.js')
 var request = require('request').defaults({
 		encoding: null
 	});
-const speech = require('@google-cloud/speech');
-const fs = require('fs');
-const speechclient = new speech.SpeechClient();
 var timestuff = require('./timestuff.js');
 const {
 	Client
@@ -369,38 +366,6 @@ client.on('message', async message => {
 		 }
 		 })
 		}	
-		}
-		if (message.attachments.array()[0].url.toLowerCase().match(/\.((mp3)|(wav))/gm)){
-		const audioBytes = body.toString('base64');
-
-// The audio file's encoding, sample rate in hertz, and BCP-47 language code
-const audio = {
-  content: audioBytes,
-};
-const config = {
-  encoding: 'LINEAR16',
-  sampleRateHertz: 16000,
-  languageCode: 'en-US',
-};
-const request = {
-  audio: audio,
-  config: config,
-};
-
-// Detects speech in the audio file
-speechclient
-  .recognize(request)
-  .then(data => {
-    const response = data[0];
-    const transcription = response.results
-      .map(result => result.alternatives[0].transcript)
-      .join('\n');
-    console.log(`Transcription: ${transcription}`);
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
-		
 		}
 		
 		});
