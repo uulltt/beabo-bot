79b749ee1d9c6718f076547b1e7dbda3d30d6f00
uulltt <ajvark@gmail.com>
2018-09-29 15:26:18 -0400

    beabo fixing edit

--- bot.js
+++ bot.js
@@ -361,10 +361,10 @@ client.on('message', async message => {
 		if (beaboMessage.toLowerCase().match(/transcribe-[a-z_]+/gm)){
 		Tesseract.recognize(body, {lang: beaboMessage.toLowerCase().match(/-[a-z_]+/gm)[0].substring(1)})
          .progress(function  (p) { console.log(JSON.stringify(p))})
-         .then(function (result) { if (result.text.length > 1996){
-		 m.edit("?en " + result.text.substring(0, 1996));
+         .then(function (result) { if (result.text.length > 2048){
+			 m.edit( {embed : { description : result.text.substring(0, 2048) }});
 		 } else {
-		 m.edit("?en " + result.text);
+		 m.edit( {embed : {description: result.text }});
 		 }
 		 })
 		} else {
