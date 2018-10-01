db150cd43e0f908c5a211499ca9e65a787c5cd73
uulltt <ajvark@gmail.com>
2018-10-01 03:07:06 -0400

    uhh yeah

--- gamesphube.js
+++ gamesphube.js
@@ -94,8 +94,8 @@ for (var i = 0; i < titles.length; i++) {
         for (var j = 0; j < title.length+1; j++) {
 		
             var last_phrase = title.slice(Math.max(0, j-lookback), j).join(' ');
-			if (last_phrase.match(/ ((of)|(the)|(in)|(a((n(d?))?)))$/gm)){
-				last_phrase = last_phrase.match(/ ((of)|(the)|(in)|(a((n(d?))?)))$/gm)[0].substring(1);
+			if (last_phrase.match(/ ((of)|([tT]he)|(in)|([aA]((n(d?))?)))$/gm)){
+				last_phrase = last_phrase.match(/ ((of)|([Tt]he)|(in)|([aA]((n(d?))?)))$/gm)[0].substring(1);
 			}
             var next_phrase = title.slice(j,j+1).join(' ');
             var map = title_map[last_phrase] || {};
@@ -103,7 +103,25 @@ for (var i = 0; i < titles.length; i++) {
             map[next_phrase] = count + 1;
             title_map[last_phrase] = map;
         }
-    }
+    } else {
+	if (title.length == 1){
+	var next_phrase = title;
+	var last_phrases = ['of', 'The', 'the', 'a', 'A', 'an', 'An', 'And', 'and'];
+	for (var j = 0; j < last_phrases.length; j++){
+		if (next_phrase.charAt(0) == 'A' || next_phrase.charAt(0) == 'E' || next_phrase.charAt(0) == 'I' || next_phrase.charAt(0) == 'O' || next_phrase.charAt(0) == 'U'){
+		if (j == 3 || j == 4)
+			continue;
+		} else {
+		if (j == 5 || j == 6)
+			continue;
+		}
+            var map = title_map[last_phrases[j]] || {};
+            var count = map[next_phrase] || 0;
+            map[next_phrase] = count + 1;
+            title_map[last_phrases[j]] = map;
+	}
+	}
+	}
 }
 console.log(title_map);
 	
@@ -126,11 +144,11 @@ for(var j = 0; j < 100 && sentences.length < 1; j++){
         while(next_word !== '') {
             sentence.push(next_word);
             var tail = sentence.slice(-lookback).join(' ');
-			if (tail.match(/ ((of)|(the)|(in)|(a((n(d?))?)))$/gm)){
-				tail = tail.match(/ ((of)|(the)|(in)|(a((n(d?))?)))$/gm)[0].substring(1);
+			if (tail.match(/ ((of)|([Tt]he)|(in)|([aA]((n(d?))?)))$/gm)){
+				tail = tail.match(/ ((of)|([Tt]he)|(in)|([aA]((n(d?))?)))$/gm)[0].substring(1);
 			}
             next_word = sample(title_map[tail]);
-			for(var k = 0; k < 100 && sentence.indexOf(next_word) != -1 && !next_word.match(/((of)|(the)|(in)|(a((n(d?))?)))$/gm); k++){
+			for(var k = 0; k < 100 && sentence.indexOf(next_word) != -1 && !next_word.match(/((of)|([Tt]he)|(in)|([aA]((n(d?))?)))$/gm); k++){
 				next_word = sample(title_map[tail]);
 			}
         }
