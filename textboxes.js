faf996c10f25c91b1176afea85349226d4a2f08a
uulltt <ajvark@gmail.com>
2019-03-02 00:03:52 -0500

    testing out new things

--- textboxes.js
+++ textboxes.js
@@ -92,6 +92,24 @@ module.exports = (message, beaboMessage, herokupg) => {
 			}
 
 		}
+		
+		if (beaboMessage.toLowerCase().startsWith('hub ')){
+		var word = message.cleanContent.substring(message.cleanContent.indexOf(" ")).trim();
+		var textCanvas = new Canvas.createCanvas(1280, 720);
+			var ctx = textCanvas.getContext("2d");
+			ctx.fillStyle = "black";
+			ctx.rect(0, 0, 1280, 720);
+			ctx.fill();
+			ctx.fillStyle = "#F7971D"
+			roundRect(ctx, 100, 5, 100, 100, 20, true);
+		message.channel.send({
+				files: [{
+						attachment: textCanvas.toBuffer(),
+						name: 'supreme.png'
+					}
+				]
+			});
+		}
 
 		if (beaboMessage.toLowerCase().startsWith("supreme ")) {
 			var word = message.cleanContent.substring(message.cleanContent.indexOf(" ")).trim();
@@ -231,4 +249,39 @@ module.exports = (message, beaboMessage, herokupg) => {
 				}
 			}
 		}	
+}
+
+function roundRect(ctx, x, y, width, height, radius, fill, stroke) {
+  if (typeof stroke == 'undefined') {
+    stroke = true;
+  }
+  if (typeof radius === 'undefined') {
+    radius = 5;
+  }
+  if (typeof radius === 'number') {
+    radius = {tl: radius, tr: radius, br: radius, bl: radius};
+  } else {
+    var defaultRadius = {tl: 0, tr: 0, br: 0, bl: 0};
+    for (var side in defaultRadius) {
+      radius[side] = radius[side] || defaultRadius[side];
+    }
+  }
+  ctx.beginPath();
+  ctx.moveTo(x + radius.tl, y);
+  ctx.lineTo(x + width - radius.tr, y);
+  ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+  ctx.lineTo(x + width, y + height - radius.br);
+  ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+  ctx.lineTo(x + radius.bl, y + height);
+  ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+  ctx.lineTo(x, y + radius.tl);
+  ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+  ctx.closePath();
+  if (fill) {
+    ctx.fill();
+  }
+  if (stroke) {
+    ctx.stroke();
+  }
+
 }
